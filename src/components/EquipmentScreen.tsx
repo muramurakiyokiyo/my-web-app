24ae4771aa6d597ecf5d9cc8c9c3a14248cb7a16
muramurakiyokiyo <kiyoichi.murata@gmail.com>
2025-12-27 18:43:21 +0900

    装備箇所のタブ切り替えに対応

--- src/components/EquipmentScreen.tsx
+++ src/components/EquipmentScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { Character } from '../types/character';
 import { armors } from '../data/armors';
 import { weapons } from '../data/weapons';
@@ -7,6 +7,8 @@ import { CalculatedStatsWindow } from './CalculatedStatsWindow';
 import { EquipmentDisplay } from './EquipmentDisplay';
 import styles from './EquipmentScreen.module.css';
 
+type EquipmentTab = 'armor' | 'rightHandWeapon' | 'leftHandWeapon';
+
 interface EquipmentScreenProps {
   character: Character;
   onEquip: (type: 'armor' | 'rightHandWeapon' | 'leftHandWeapon', id: number) => void;
@@ -14,6 +16,7 @@ interface EquipmentScreenProps {
 }
 
 export const EquipmentScreen: React.FC<EquipmentScreenProps> = ({ character, onEquip, onClose }) => {
+  const [activeTab, setActiveTab] = useState<EquipmentTab>('armor');
   const calculatedStats = getCalculatedStats(character);
   return (
     <div className={styles.equipmentScreen}>
@@ -36,70 +39,92 @@ export const EquipmentScreen: React.FC<EquipmentScreenProps> = ({ character, onE
 
         <div className={styles.equipmentScreenBody}>
           <div className={styles.equipmentListSection}>
-            <div className={styles.equipmentCategory}>
-              <h3 className={styles.equipmentListTitle}>防具</h3>
-              <div className={styles.equipmentList}>
-                {armors.map((armor) => {
-                  const isEquipped = character.equipment?.armor === armor.id;
-                  return (
-                    <button
-                      key={armor.id}
-                      className={`${styles.equipmentItem} ${isEquipped ? styles.equipmentItemEquipped : ''}`}
-                      onClick={() => onEquip('armor', armor.id)}
-                    >
-                      <div className={styles.equipmentItemHeader}>
-                        <div className={styles.equipmentItemName}>{armor.name}</div>
-                        {isEquipped && <div className={styles.equipmentItemEquippedBadge}>装備中</div>}
-                      </div>
-                      <div className={styles.equipmentItemStat}>防御力: +{armor.defence}</div>
-                    </button>
-                  );
-                })}
-              </div>
+            {/* タブ */}
+            <div className={styles.tabContainer}>
+              <button
+                className={`${styles.tab} ${activeTab === 'armor' ? styles.tabActive : ''}`}
+                onClick={() => setActiveTab('armor')}
+              >
+                防具
+              </button>
+              <button
+                className={`${styles.tab} ${activeTab === 'rightHandWeapon' ? styles.tabActive : ''}`}
+                onClick={() => setActiveTab('rightHandWeapon')}
+              >
+                右手武器
+              </button>
+              <button
+                className={`${styles.tab} ${activeTab === 'leftHandWeapon' ? styles.tabActive : ''}`}
+                onClick={() => setActiveTab('leftHandWeapon')}
+              >
+                左手武器
+              </button>
             </div>
 
-            <div className={styles.equipmentCategory}>
-              <h3 className={styles.equipmentListTitle}>右手武器</h3>
-              <div className={styles.equipmentList}>
-                {weapons.map((weapon) => {
-                  const isEquipped = character.equipment?.rightHandWeapon === weapon.id;
-                  return (
-                    <button
-                      key={weapon.id}
-                      className={`${styles.equipmentItem} ${isEquipped ? styles.equipmentItemEquipped : ''}`}
-                      onClick={() => onEquip('rightHandWeapon', weapon.id)}
-                    >
-                      <div className={styles.equipmentItemHeader}>
-                        <div className={styles.equipmentItemName}>{weapon.name}</div>
-                        {isEquipped && <div className={styles.equipmentItemEquippedBadge}>装備中</div>}
-                      </div>
-                      <div className={styles.equipmentItemStat}>攻撃力: +{weapon.attack}</div>
-                    </button>
-                  );
-                })}
-              </div>
-            </div>
+            {/* タブコンテンツ */}
+            <div className={styles.tabContent}>
+              {activeTab === 'armor' && (
+                <div className={styles.equipmentList}>
+                  {armors.map((armor) => {
+                    const isEquipped = character.equipment?.armor === armor.id;
+                    return (
+                      <button
+                        key={armor.id}
+                        className={`${styles.equipmentItem} ${isEquipped ? styles.equipmentItemEquipped : ''}`}
+                        onClick={() => onEquip('armor', armor.id)}
+                      >
+                        <div className={styles.equipmentItemHeader}>
+                          <div className={styles.equipmentItemName}>{armor.name}</div>
+                          {isEquipped && <div className={styles.equipmentItemEquippedBadge}>装備中</div>}
+                        </div>
+                        <div className={styles.equipmentItemStat}>防御力: +{armor.defence}</div>
+                      </button>
+                    );
+                  })}
+                </div>
+              )}
 
-            <div className={styles.equipmentCategory}>
-              <h3 className={styles.equipmentListTitle}>左手武器</h3>
-              <div className={styles.equipmentList}>
-                {weapons.map((weapon) => {
-                  const isEquipped = character.equipment?.leftHandWeapon === weapon.id;
-                  return (
-                    <button
-                      key={weapon.id}
-                      className={`${styles.equipmentItem} ${isEquipped ? styles.equipmentItemEquipped : ''}`}
-                      onClick={() => onEquip('leftHandWeapon', weapon.id)}
-                    >
-                      <div className={styles.equipmentItemHeader}>
-                        <div className={styles.equipmentItemName}>{weapon.name}</div>
-                        {isEquipped && <div className={styles.equipmentItemEquippedBadge}>装備中</div>}
-                      </div>
-                      <div className={styles.equipmentItemStat}>攻撃力: +{weapon.attack}</div>
-                    </button>
-                  );
-                })}
-              </div>
+              {activeTab === 'rightHandWeapon' && (
+                <div className={styles.equipmentList}>
+                  {weapons.map((weapon) => {
+                    const isEquipped = character.equipment?.rightHandWeapon === weapon.id;
+                    return (
+                      <button
+                        key={weapon.id}
+                        className={`${styles.equipmentItem} ${isEquipped ? styles.equipmentItemEquipped : ''}`}
+                        onClick={() => onEquip('rightHandWeapon', weapon.id)}
+                      >
+                        <div className={styles.equipmentItemHeader}>
+                          <div className={styles.equipmentItemName}>{weapon.name}</div>
+                          {isEquipped && <div className={styles.equipmentItemEquippedBadge}>装備中</div>}
+                        </div>
+                        <div className={styles.equipmentItemStat}>攻撃力: +{weapon.attack}</div>
+                      </button>
+                    );
+                  })}
+                </div>
+              )}
+
+              {activeTab === 'leftHandWeapon' && (
+                <div className={styles.equipmentList}>
+                  {weapons.map((weapon) => {
+                    const isEquipped = character.equipment?.leftHandWeapon === weapon.id;
+                    return (
+                      <button
+                        key={weapon.id}
+                        className={`${styles.equipmentItem} ${isEquipped ? styles.equipmentItemEquipped : ''}`}
+                        onClick={() => onEquip('leftHandWeapon', weapon.id)}
+                      >
+                        <div className={styles.equipmentItemHeader}>
+                          <div className={styles.equipmentItemName}>{weapon.name}</div>
+                          {isEquipped && <div className={styles.equipmentItemEquippedBadge}>装備中</div>}
+                        </div>
+                        <div className={styles.equipmentItemStat}>攻撃力: +{weapon.attack}</div>
+                      </button>
+                    );
+                  })}
+                </div>
+              )}
             </div>
           </div>
 
